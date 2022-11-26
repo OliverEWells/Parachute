c402e5d2f7194db55468608d45278f2af4f40cf5
coleharris2015 <coleharris2015@gmail.com>
2022-11-26 16:42:38 -0700

    gitignore added

--- Program.cs
+++ Program.cs
@@ -8,14 +8,25 @@ word word = new word();
 
 word.the_word = word.random_word();
 
-Console.Write(word.the_word);
+//Console.Write(word.the_word);
 
-int guesses_left = 4;
+Console.Write("Guess a letter");
+
+string guess = Console.ReadLine();
+
+Console.Write(guess);
+
 parachute parachute = new parachute();
+
+int guesses_left = -1;
+while(guesses_left <= 4){
+
 
 Console.WriteLine();
 parachute.print_parachute(guesses_left);
+guesses_left+=1;
 
+}
 
 
 }
